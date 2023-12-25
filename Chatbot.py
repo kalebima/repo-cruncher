2c68fbb76459cffef74edc19065355998bf8231e
Matt Kalebic <matt.kalebic@gmail.com>
2023-12-24 22:42:49 -0500

    add Streamlit fix

--- Chatbot.py
+++ Chatbot.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import ChatMessage
